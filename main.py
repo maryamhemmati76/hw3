bb6ef90317bca4e9082019b80bc1b92e9a4b3ec8
maryamhemmati76 <maryamhemmati0736@gmail.com>
2024-04-20 10:17:52 +0330

    new hw3_1 is added

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 from src.hw3_1 import find_word
 word_list = ["banana", "apple", "cherry", "grape", "kiwi", "date"]
 find_word(word_list)
+
+word_list = ["new york", "toronto", "london", "greece"]
+find_word(word_list)
+
